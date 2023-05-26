b4678584a51714918d857235642dd96ac46f5550
devminoh <gmerit0116@naver.com>
2023-05-26 13:59:07 +0900

    :sparkles: [Feat] 사이드바 링크 연결"

--- front/styles/layout.ts
+++ front/styles/layout.ts
@@ -1,40 +1,54 @@
 import styled from 'styled-components';
-import { Layout, Breadcrumb } from 'antd';
+import { Layout, Breadcrumb, Input } from 'antd';
 import Link from 'next/link';
 
 const { Content, Sider } = Layout;
+
+export const Container = styled(Layout)`
+  height: 100vh;
+`;
 
 export const Head = styled.div`
   min-height: 10vh;
   padding: 10px;
   background-color: white;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
   a {
-    color: black;
+    color: #a4d0a4;
     font-size: 30px;
     font-weight: 700;
     text-decoration: none;
+    margin-top: 20px;
+
+    &:hover {
+      color: #617a55;
+    }
+  }
+
+  div {
+    width: 80vw;
+    display: flex;
+    justify-content: flex-end;
+    margin: 10px 0;
   }
 `;
 
 export const Title = styled(Link)`
   display: flex;
   align-items: center;
-
-  :hover {
-    color: blue;
-  }
 `;
 
 export const ContentContainer = styled(Content)`
   margin: 0 16px;
+  height: 80vh;
 
   div{
     padding: 24px;
-    min-height: 360px;
+    min-height: 100px;
     background-color: white;
   }
 `;
@@ -45,4 +59,11 @@ export const SubTab = styled(Breadcrumb)`
 
 export const SideBar = styled(Sider)`
   background-color: white !important;
+`;
+
+export const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+  display: flex;
+  width: 300px;
+  margin-right: 20px;
 `;
